02f7bfeceb5d48754102034e22040d5b2c56e329
Tim Kelly <timpkelly1@gmail.com>
2021-08-21 14:55:35 +1000

    Updates

---
+++ precache-manifest.4a9211cb4455102ffb47b3ac531d8366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121d3cbc1445c825fa5a7edf14b81dc2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c0491d8e5f83e63ff3a",
+    "url": "./static/css/main.9b685743.chunk.css"
+  },
+  {
+    "revision": "c1239aca91e27461db14",
+    "url": "./static/js/2.65d033f0.chunk.js"
+  },
+  {
+    "revision": "5c0491d8e5f83e63ff3a",
+    "url": "./static/js/main.95ee0eda.chunk.js"
+  },
+  {
+    "revision": "b164257a37f695dd0fef",
+    "url": "./static/js/runtime-main.fb6b5b82.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web3-app-solidity-ui/precache-manifest.91ba3742cfb8db4ce918d787c81acbe5.js"
+  "./precache-manifest.4a9211cb4455102ffb47b3ac531d8366.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web3-app-solidity-ui/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
